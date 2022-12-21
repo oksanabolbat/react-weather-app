377cdf5e0b43b94998bc3b4ccfadc5a37ada30d8
oksanabolbat <oksana.bolbat@gmail.com>
2022-12-21 21:05:29 +0200

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,8 @@ const Footer = () => {
   return (
     <footer>
       This project was coded by Oksana Bolbat and is open-sourced on{' '}
-      <a href="https://github.com/oksanabolbat/weather-app-vanilla-js">
-        GitHub
-      </a>{' '}
-      and hosted on <a href="https://www.netlify.com/">Netlify</a>
+      <a href="https://github.com/oksanabolbat/react-weather-app">GitHub</a> and
+      hosted on <a href="https://www.netlify.com/">Netlify</a>
     </footer>
   );
 };
